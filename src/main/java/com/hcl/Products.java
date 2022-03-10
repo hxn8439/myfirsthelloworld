16247307d3bbb156e756f6dbba71bda38fc4da5c
Hamilton Nguyen <52980606+hxn8439@users.noreply.github.com>
2022-03-10 16:22:31 -0600

    products java file

---
+++ src/main/java/com/hcl/Products.java
@@ -0,0 +1,23 @@
+package com.hcl;
+
+public class Products {
+
+	//You need to build a menu driven “Create/ Read/ Update/ Delete” aka CRUD application, 
+	//where you can create, update, delete, view all Products.
+	//Use collections streams technique to show products, filter products etc.
+	//For update and delete, you can prompt id that is unique and operate on that.
+	//Krishna
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	
+	
+}
